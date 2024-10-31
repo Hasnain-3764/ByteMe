3adf4c319c943061c2965109b55558042a1f16e6
hasnain <mdhasnain85629@gmil.com>
2024-10-31 16:58:26 +0530

    added admin methods
    in terminal interface, added admin methods like updateItems, addNewItems, removeItems etc

--- src/TerminalInterface.java
+++ src/TerminalInterface.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Scanner;
 
 public class TerminalInterface {
@@ -22,9 +23,10 @@ public class TerminalInterface {
             case 3 -> showRegularCustomerMenu();
             case 4 -> System.out.println("Exiting... Thank You");
             default -> System.out.println("Invalid choice. Try again.");
-
         }
     }
+
+    // Admin functions
     public void showAdminMenu(){
         while(true){
             System.out.println("\nAdmin Menu:");
@@ -40,11 +42,10 @@ public class TerminalInterface {
 
             switch(choice){
                 case 1 -> menuService.getAllItems().forEach(System.out::println);
-//                case 2 -> addNewItem();
-//                case 3 -> updateItem();
-//                case 4 -> removeItem();
-//                case 5 -> generateSalesReport();
-                // to be implemented.
+                case 2 -> addNewItem();
+                case 3 -> updateItem();
+                case 4 -> removeItem();
+//                case 5 -> generateSalesReport(); // to be implemented.
                 case 6 -> showOrderTracking();
                 case 7 -> {
                     System.out.println("Logging out...");
@@ -52,21 +53,83 @@ public class TerminalInterface {
                 }
                 default -> System.out.println("Invalid choice, try again");
             }
-
         }
+    }
+
+    // this func will call addMenuItem in menuServices
+    private void addNewItem(){
+        System.out.println("Enter item name: ");
+        String name = scanner.next();
+        System.out.println("Enter item price: ");
+        double price = scanner.nextDouble();
+        System.out.println("Enter item type (e.g. Meal, Snack, Beverages, Dessert): ");
+        String type = scanner.next();
+        System.out.println("Is the item available? (true/false)");
+        boolean availability = scanner.nextBoolean();
+
+        MenuItem item = new MenuItem(name, price, type, availability);
+        menuService.addMenuItem(item);
+        System.out.println("Item added successfully");
+    }
+
+    private void updateItem(){
+        System.out.println("Enter the name of the item to update: ");
+        String name = scanner.next();
+        MenuItem item = menuService.searchItems(name).stream().findFirst().orElse(null);
+        if(item == null){
+            System.out.println("Item not found.");
+            return;
+        }
+        System.out.print("Enter new price: ");
+        double price = scanner.nextDouble();
+        System.out.println("Enter new type: ");
+        String type = scanner.next();
+        System.out.println("Is the item available? (true/false): ");
+        boolean availability = scanner.nextBoolean();
+
+        menuService.updateMenuItem(new MenuItem(name,price,type,availability));
+        System.out.println("Item updated succesfully");
 
     }
 
+    private void removeItem(){
+        System.out.println("Enter the name of the item to remove: ");
+        String name = scanner.next();
+        menuService.removeMenuItem(name);
+        System.out.println("Item removed successfully.");
+    }
 
+    public void showVIPCustomerMenu(){
+        while(true){
+            System.out.println("\nRegular Customer Menu:");
+            System.out.println("1. Browse Menu");
+            System.out.println("2. Search Menu Items");
+            System.out.println("3. Place an Order");
+            System.out.println("4. View Order History");
+            System.out.println("5. Logout");
 
-    public void showVIPCustomerMenu(){ System.out.println("this is VIP menu");}
+            int choice = InputUtils.readInt("Enter your choice: ", 1, 5);
+
+            switch(choice){
+
+            }
+        }
+    }
+
     public void showRegularCustomerMenu(){
         System.out.println("this is customer menu");
     }
+
+
     public void showOrderTracking(){
         System.out.println("this is order tracking menu");
     }
     public void handleNavigation(){
         System.out.println("this is navigation handle");
     }
+
+    private void generateSalesReport(){
+        System.out.println("Generating sales report...");
+    }
+
 }
