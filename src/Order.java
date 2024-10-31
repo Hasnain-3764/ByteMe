bd20e5f43834cac55be6af9a648db28226f5f67b
hasnain <mdhasnain85629@gmil.com>
2024-11-01 01:18:41 +0530

    created ordercomparator, updated order,ordermanagerimpl
    created ordercomparator, updated order,ordermanagerimpl to build upon existing logic

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,14 @@
-public class Order {
+public class Order implements Comparable<Order> {
+    public enum Priority {
+        HIGH, NORMAL
+    }
+
+    private String customerID;
+    private Priority priority;
+    // other order details...
+
+    @Override
+    public int compareTo(Order other) {
+        return this.priority.compareTo(other.priority);
+    }
 }
--- src/OrderManagerImpl.java
+++ src/OrderManagerImpl.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class OrderManagerImpl implements OrderManager {
     private static OrderManagerImpl instance;
+    private List<Order> pendingOrders;
+    private Map<String, List<Order>> orderHistories;
 
     private OrderManagerImpl() {
-
+//        pendingOrders = new PriorityQueue<>(new OrderComparator()); // to be implemented
+//        orderHistories = new HashMap<>();
     }
 
     public static OrderManagerImpl getInstance(){
@@ -10,5 +15,15 @@ public class OrderManagerImpl implements OrderManager {
             instance = new OrderManagerImpl();
         }
         return instance;
+    }
+
+    @Override
+    public void placeOrder(Order order, boolean isVIP){
+        System.out.println("Order placed successfully");
+    }
+
+    @Override
+    public List<Order> getOrderHistory(String customerID) {
+        return orderHistories.getOrDefault(customerID, new ArrayList<>());
     }
 }
